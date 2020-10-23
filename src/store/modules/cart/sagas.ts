f5b0f82387f81cbc51cf3c3f05911a89cea80b37
jgabriel1 <josegabriel@det.ufc.br>
2020-10-23 15:53:34 -0300

    Listening to success and failue based on the stock quantity for the product.

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,9 +1,20 @@
-import { all, select, takeLatest } from 'redux-saga/effects';
+import { AxiosResponse } from 'axios';
+import { all, select, takeLatest, call, put } from 'redux-saga/effects';
 
 import { IState } from '../..';
-import { addProductToCartRequest } from './actions';
+import api from '../../../services/api';
+import {
+  addProductToCartFailure,
+  addProductToCartRequest,
+  addProductToCartSuccess,
+} from './actions';
 
 type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>;
+
+interface IStockResponse {
+  id: number;
+  quantity: number;
+}
 
 function* checkProductStock({ payload }: CheckProductStockRequest) {
   const { product } = payload;
@@ -14,6 +25,17 @@ function* checkProductStock({ payload }: CheckProductStockRequest) {
       0
     );
   });
+
+  const availableStockResponse: AxiosResponse<IStockResponse> = yield call(
+    api.get,
+    `stock/${product.id}`,
+  );
+
+  if (availableStockResponse.data.quantity > currentQuantity) {
+    yield put(addProductToCartSuccess(product));
+  } else {
+    yield put(addProductToCartFailure(product.id));
+  }
 
   console.log(currentQuantity);
 }
